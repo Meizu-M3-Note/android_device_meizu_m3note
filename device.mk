eaf12cf71d84779ad65dac0cad8c2f0c03c5fe1b
jmpfbmx <jmpf.bmx@gmail.com>
2021-07-28 12:12:59 +0200

    m3note: Initial TWRP tree
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I3d54f7afd78cefe1ab51bb73143d795ce89fac50

---
+++ device.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := device/meizu/m3note
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/Image.gz-dtb:Image.gz-dtb \
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.mt6755.rc:root/init.recovery.mt6755.rc \
+    $(LOCAL_PATH)/rootdir/sbin/healthd:root/sbin/healthd
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_NAME := m3note
---
+++ omni_m3note.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/meizu/m3note/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m3note
+PRODUCT_NAME := omni_m3note
+PRODUCT_BRAND := MEIZU
+PRODUCT_MANUFACTURER := MEIZU
+PRODUCT_MODEL := M3 Note
