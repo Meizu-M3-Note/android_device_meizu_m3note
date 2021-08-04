bdadf343975705f1cf0cb5179d650c9ebf212891
jmpfbmx <jmpf.bmx@gmail.com>
2021-08-05 00:26:25 +0200

    m3note: Switch to lineage
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I19bcdb934ba447fa655a8c479cd6007a3b846653

--- lineage.mk
+++ lineage.mk
@@ -1,8 +1,24 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -11,7 +27,7 @@ $(call inherit-product, device/meizu/m3note/device.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_RELEASE_NAME := Meizu M3 Note
 PRODUCT_DEVICE := m3note
-PRODUCT_NAME := cm_m3note
+PRODUCT_NAME := lineage_m3note
 PRODUCT_BRAND := Meizu
 PRODUCT_MODEL := M3 Note
 PRODUCT_MANUFACTURER := Meizu
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,5 +21,5 @@
 # function: add_lunch_combo generic-eng
 
 for var in eng user userdebug; do
-  add_lunch_combo cm_m3note-$var
+  add_lunch_combo lineage_m3note-$var
 done
