7233fa57c32cbb583e7503c6244897ac6730e3e2
jmpfbmx <jmpf.bmx@gmail.com>
2021-08-05 00:24:01 +0200

    m3note: Set correct fingerprint permission file name
     * Cyanogenmod one was used at cm-12.1
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I614e7a518a44c858276d009dbc59b9aae30e5cd9

--- device.mk
+++ device.mk
@@ -147,7 +147,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/cyanogenmod.hardware.fingerprint.xml:system/etc/permissions/cyanogenmod.hardware.fingerprint.xml
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 # Power
 PRODUCT_PACKAGES += \
