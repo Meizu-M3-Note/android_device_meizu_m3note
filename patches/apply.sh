d3a5c33236f8e462dc38cfc9abe9e46f2c3497eb
daniel_hk <daniel.p6800@gmail.com>
2021-08-04 23:59:11 +0200

    m3note: liblog: Add needed MTK log functions
    1. Add the persist.ril.log property.
       Set to 1 to move RLOG's to main.
    2. Add the function __xlog_buf_printf() required by MTK blobs.
    3. Add the ro.disable.xlog property.
       Set to value other than 0 to disable the xlog.
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I4457211a80f9c8aa63295e9cf8074c44aa30dbb7

--- patches/apply.sh
+++ patches/apply.sh
@@ -5,4 +5,5 @@ cd ../../../..
 echo -e "${BBLUE}We are gonna patch system/core${NC}"
 cd system/core
 patch -p1 < ../../device/meizu/m3note/patches/system/core/0001-init-Re-allow-services-without-SELinux-contexts-defi.patch
+patch -p1 < ../../device/meizu/m3note/patches/system/core/0001-liblog-Add-needed-MTK-log-functions.patch
 cd ../..
