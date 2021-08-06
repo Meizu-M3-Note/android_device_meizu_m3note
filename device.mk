a120f97c1b5126ba2e89839388aca6e0f5c51ca0
jmpfbmx <jmpf.bmx@gmail.com>
2021-08-06 11:40:08 +0200

    m3note: Import init.recovery.usb.rc
     * From XPERIA XA, since at Stock ROM Recovery is missing
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I38a8048afd32d48ef3a22d21ef171a927ddf36ad

--- device.mk
+++ device.mk
@@ -9,6 +9,7 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/Image.gz-dtb:Image.gz-dtb \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.mt6755.rc:root/init.recovery.mt6755.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.usb.rc:root/init.recovery.usb.rc \
     $(LOCAL_PATH)/rootdir/sbin/healthd:root/sbin/healthd
 
 $(call inherit-product, build/target/product/full.mk)
