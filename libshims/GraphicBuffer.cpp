2490a32d51166e729a4713cec2056e9820433f75
jmpfbmx <jmpf.bmx@gmail.com>
2021-08-07 01:42:28 +0200

    m3note: libshims: Shim _ZN7android18BufferItemConsumer* missing symbols
     * It fixes:
       * E HAL     : dlopen failed: cannot locate symbol "_ZN7android18BufferItemConsumer20setDefaultBufferSizeEjj" referenced by "/system/lib/libmmsdkservice.feature.so"...
       * E HAL     : dlopen failed: cannot locate symbol "_ZN7android18BufferItemConsumer22setDefaultBufferFormatEj" referenced by "/system/lib/libmmsdkservice.feature.so"...
       * E HAL     : dlopen failed: cannot locate symbol "_ZN7android18BufferItemConsumer13acquireBufferEPNS_22IGraphicBufferConsumer10BufferItemExb" referenced by "/system/lib/libmmsdkservice.feature.so"...
       * E HAL     : dlopen failed: cannot locate symbol "_ZN7android18BufferItemConsumer13releaseBufferERKNS_22IGraphicBufferConsumer10BufferItemERKNS_2spINS_5FenceEEE" referenced by "/system/lib/libmmsdkservice.feature.so"...
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I63c7b12f43967850fffa6b265877cc72de149b09

--- libshims/GraphicBuffer.cpp
+++ libshims/GraphicBuffer.cpp
@@ -50,4 +50,8 @@ GraphicBuffer::GraphicBuffer(uint32_t inWidth, uint32_t inHeight,
 
 extern "C" {
     void _ZN7android22IGraphicBufferConsumer10BufferItemC1Ev () {}
+    void _ZN7android18BufferItemConsumer20setDefaultBufferSizeEjj () {}
+    void _ZN7android18BufferItemConsumer22setDefaultBufferFormatEj () {}
+    void _ZN7android18BufferItemConsumer13acquireBufferEPNS_22IGraphicBufferConsumer10BufferItemExb () {}
+    void _ZN7android18BufferItemConsumer13releaseBufferERKNS_22IGraphicBufferConsumer10BufferItemERKNS_2spINS_5FenceEEE () {}
 }
