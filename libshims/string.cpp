e6ab93b28f8587d3ad878765d75b147309054ce0
R0rt1z2 <rogerortizleal@gmail.com>
2021-08-07 12:13:55 +0200

    m3note: Update String16 shim
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I0a7aed105cc5a828e4a96f3e6c5dc8feb0f504e3

--- libshims/string.cpp
+++ libshims/string.cpp
@@ -14,12 +14,12 @@
  * limitations under the License.
  */
 
-extern "C" int _ZN7android8String16C1EPKDsj();
+extern "C" int _ZN7android8String16C1EPKDsj() {return 0;}
 extern "C" int _ZN7android8String16C1EPKtj() {
     return _ZN7android8String16C1EPKDsj();
 }
 
-extern "C" int _ZN7android8String165setToEPKDsj();
+extern "C" int _ZN7android8String165setToEPKDsj() {return 0;}
 extern "C" int _ZN7android8String165setToEPKtj() {
     return _ZN7android8String165setToEPKDsj();
 }
